de6abad6779f633296a4ef79be2d74c2fbd84e77
Nikos Vaggalis <nikos.vaggalis@gmail.com>
2025-01-24 15:44:28 +0200

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import Presentation from '@/components/Presentation';
+
+const Home: NextPage = () => {
+  return (
+    <div >
+      <Presentation />
+    </div>
+  );
+};
+
+export default Home;
